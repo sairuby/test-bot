f57ccb1062ad21b9bf210d103031fb70b8c17e9d
sairuby <vnsaitheja@gmail.com>
2018-02-06 22:09:42 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -16,21 +16,3 @@ get '/hello' do
 	" hello"
 end
 
-
-# Slack.configure do |config|
-#   config.token = "xoxb-309560904657-OOuZw7YL08EtPa8E82WdewCG"
-# end
-
-
-# require 'slack-ruby-client'
-
-# Slack.configure do |config|
-#   config.token = "xoxb-309560904657-OOuZw7YL08EtPa8E82WdewCG"
-#   raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
-# end
-
-# client = Slack::Web::Client.new
-
-# client.auth_test
-
-# client.chat_postMessage(channel: '#general', text: 'Hello World', as_user: true)
