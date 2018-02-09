f446178cd8012f8e78ce743304b7c0a9a5c16321
sairuby <vnsaitheja@gmail.com>
2018-02-09 13:47:09 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -12,12 +12,12 @@ post '/testcall' do
 	#binding.pry
 	a = request.params['text']
 	b = request.params['trigger_word']
-	h = a.slice! b
-	"#{h}"
-	puts "#{h}"
+	#h = a.slice! b
+	#"#{h}"
+	#puts "#{h}"
   content_type :json
   #{ :text => "#{request.content_type} \n  #{request.params.class} #{request.params['text']} #{request.params['trigger_word']}"}.to_json
-  {:text => "#{h}"}.to_json
+  {:text => "hello"}.to_json
 end
 get '/hello' do
 	" hello"
