301869edb2befde7e959ad760a26a126ec944096
sairuby <vnsaitheja@gmail.com>
2018-02-09 13:21:18 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -10,20 +10,12 @@ require 'pry'
 
 post '/testcall' do
 	#binding.pry
-	#request.body.read
-	#puts " hello"
- # puts " hello in post call"
-  #"hello in postcall"
-  #{
-   # "text": "My response"
-  #
-	#binding.pry
-	#"#{session.inspect}
-	h = "hello"	
-	h = request.params['text'].slice! request.params['trigger_word']
+	a = request.params['text']
+	b = request.params['trigger_word']
+	h = a.slice! b
   content_type :json
   #{ :text => "#{request.content_type} \n  #{request.params.class} #{request.params['text']} #{request.params['trigger_word']}"}.to_json
-  {text => "#{h}"}.to_json
+  {:text => "#{h}"}.to_json
 end
 get '/hello' do
 	" hello"
