8059b85687c2b7cc98accad9f55ab5db334a0bf2
sairuby <vnsaitheja@gmail.com>
2018-02-13 11:36:17 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -23,7 +23,7 @@ post '/testcall' do
 	request = Net::HTTP::Get.new(url)
 	request["Authorization"] = 'Basic NTAyNDEyMjMyOlMzcnZpYzNuMHc='
 	response = http.request(request)
-	a = response.read_body
+	data1 = response.read_body
 	#url1 = "https://geapplight.service-now.com/api/now/v1/table/cmdb_ci_service?sysparm_query=name=#{a}&sysparm_fields=u_technical_contact_sso_1&displayvalue=true"
 	#uri = URI(a)
 	#http.use_ssl = true
@@ -35,7 +35,7 @@ post '/testcall' do
 	#puts "#{h}"
   content_type :json
   #{ :text => "#{request.content_type} \n  #{request.params.class} #{request.params['text']} #{request.params['trigger_word']}"}.to_json
-  {:text => "#{a}"}.to_json
+  {:text => "#{data1}"}.to_json
 end
 get '/hello' do
 	" hello"
