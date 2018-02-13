0b64d77777b9838ace03d0e863564e6ff2377812
sairuby <vnsaitheja@gmail.com>
2018-02-13 11:52:59 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -12,15 +12,23 @@ require 'net/http'
 
 post '/testcall' do
 	#binding.pry
-	a = request.params['text']
-	b = request.params['trigger_word']
-	h = a.slice! b
+	#a = request.params['text']
+	#b = request.params['trigger_word']
+	#h = a.slice! b
      
-	url = URI("https://geapplight.service-now.com/api/now/v1/table/cmdb_ci_service?sysparm_query=name=NPI&sysparm_fields=u_technical_contact_sso_1&displayvalue=true")
+	url = URI("https://geapplight.service-now.com/api/now/v1/table/cmdb_ci_service?sysparm_query=name%3DNPI&sysparm_fields=u_technical_contact_sso_1&displayvalue=true")
+
 	http = Net::HTTP.new(url.host, url.port)
-	request = Net::HTTP::Get.new(url)
-	request["Authorization"] = 'Basic NTAyNDEyMjMyOlMzcnZpYzNuMHc='
-	response = http.request(request)
+	http.use_ssl = true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+request = Net::HTTP::Get.new(url)
+request["authorization"] = 'Basic NTAyNDEyMjMyOlMzcnZpYzNuMHc='
+request["cache-control"] = 'no-cache'
+request["postman-token"] = 'e5389d56-52fc-1e23-50e7-8b4f215ff28f'
+
+response = http.request(request)
+puts response.read_body
 	data1 = JSON.parse(response.read_body)
 	data2 = "hello"
   	content_type :json
