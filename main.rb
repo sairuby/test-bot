1b67ef18e330c243bafe2c378adb75f041cabfd2
sairuby <vnsaitheja@gmail.com>
2018-02-13 11:47:25 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -17,25 +17,14 @@ post '/testcall' do
 	h = a.slice! b
      
 	url = URI("https://geapplight.service-now.com/api/now/v1/table/cmdb_ci_service?sysparm_query=name=NPI&sysparm_fields=u_technical_contact_sso_1&displayvalue=true")
-
 	http = Net::HTTP.new(url.host, url.port)
-
 	request = Net::HTTP::Get.new(url)
 	request["Authorization"] = 'Basic NTAyNDEyMjMyOlMzcnZpYzNuMHc='
 	response = http.request(request)
 	data1 = JSON.parse(response.read_body)
-	#url1 = "https://geapplight.service-now.com/api/now/v1/table/cmdb_ci_service?sysparm_query=name=#{a}&sysparm_fields=u_technical_contact_sso_1&displayvalue=true"
-	#uri = URI(a)
-	#http.use_ssl = true
-    	#http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-	
-
-	
-	#"#{h}"
-	#puts "#{h}"
-  content_type :json
-  #{ :text => "#{request.content_type} \n  #{request.params.class} #{request.params['text']} #{request.params['trigger_word']}"}.to_json
-  {:text => "#{data1}"}.to_json
+	data2 = "hello"
+  	content_type :json
+   	{:text => "#{data2}"}.to_json
 end
 get '/hello' do
 	" hello"
