de5144a07d4ac87d2ee02f9b5205f12d5ddb04e2
sairuby <vnsaitheja@gmail.com>
2018-02-13 11:34:24 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,13 +6,31 @@ require 'logger'
 require 'yaml'
 require 'fileutils'
 require 'pry'
+require 'uri'
+require 'net/http'
 
 
 post '/testcall' do
-	#binding.pry
+	binding.pry
 	a = request.params['text']
 	b = request.params['trigger_word']
 	h = a.slice! b
+     
+	url = URI("https://geapplight.service-now.com/api/now/v1/table/cmdb_ci_service?sysparm_query=name=NPI&sysparm_fields=u_technical_contact_sso_1&displayvalue=true")
+
+	http = Net::HTTP.new(url.host, url.port)
+
+	request = Net::HTTP::Get.new(url)
+	request["Authorization"] = 'Basic NTAyNDEyMjMyOlMzcnZpYzNuMHc='
+	response = http.request(request)
+	a = response.read_body
+	#url1 = "https://geapplight.service-now.com/api/now/v1/table/cmdb_ci_service?sysparm_query=name=#{a}&sysparm_fields=u_technical_contact_sso_1&displayvalue=true"
+	#uri = URI(a)
+	#http.use_ssl = true
+    	#http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	
+
+	
 	#"#{h}"
 	#puts "#{h}"
   content_type :json
